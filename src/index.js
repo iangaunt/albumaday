cb046740feafa171791d2bc2cb16f2bd9de952b8
Ian G <66448861+iangaunt@users.noreply.github.com>
2023-07-24 21:02:13 -0700

    Swapped function location

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { newAlbum, colorMode } from './data.js';
 window.newAlbum = newAlbum;
 window.colorMode = colorMode;
 
-document.getElementById("header").addEventListener("click", newAlbum);
-document.getElementById("header").addEventListener("touchend", newAlbum);
+document.getElementById("main").addEventListener("click", newAlbum);
+document.getElementById("main").addEventListener("touchend", newAlbum);
 
-document.getElementById("main").addEventListener("click", colorMode);
-document.getElementById("main").addEventListener("touchend", colorMode);
+document.getElementById("header").addEventListener("click", colorMode);
+document.getElementById("header").addEventListener("touchend", colorMode);
