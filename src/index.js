9b6e926b1c4f6c095f526e738a63600f86232428
Ian G <66448861+iangaunt@users.noreply.github.com>
2023-07-24 20:46:17 -0700

    Adjustments to z-index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import { newAlbum, colorMode } from './data.js';
 window.newAlbum = newAlbum;
 window.colorMode = colorMode;
 
-document.getElementById("main").addEventListener("click", newAlbum);
-document.getElementById("main").addEventListener("touchend", newAlbum);
+document.getElementById("album-cover").addEventListener("click", newAlbum);
+document.getElementById("album-cover").addEventListener("touchend", newAlbum);
 
-document.getElementById("header").addEventListener("click", colorMode);
-document.getElementById("header").addEventListener("touchend", colorMode);
+document.getElementById("logo").addEventListener("click", colorMode);
+document.getElementById("logo").addEventListener("touchend", colorMode);
