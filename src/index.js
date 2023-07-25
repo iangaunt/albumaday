0263117986d71d767fef8b9e970396eb6835e60c
Ian G <66448861+iangaunt@users.noreply.github.com>
2023-07-24 19:23:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { newAlbum, colorMode } from './data.js';
 window.newAlbum = newAlbum;
 window.colorMode = colorMode;
 
-document.getElementById("album-cover").addEventListener("onmouseup", newAlbum);
-document.getElementById("logo").addEventListener("onmouseup", colorMode);
+document.getElementById("album-cover").addEventListener("onclick", newAlbum);
+document.getElementById("logo").addEventListener("onclick", colorMode);
