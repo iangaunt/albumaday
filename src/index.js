895db1f6d9a17e151e96662c404fa539accc36ff
Ian G <66448861+iangaunt@users.noreply.github.com>
2023-07-24 20:24:52 -0700

    Touch events

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import { newAlbum, colorMode } from './data.js';
 window.newAlbum = newAlbum;
 window.colorMode = colorMode;
 
-document.getElementById("album-cover").addEventListener("onmouseup", newAlbum);
-document.getElementById("logo").addEventListener("onmouseup", colorMode);
+document.getElementById("album-cover").addEventListener("click", newAlbum);
+document.getElementById("album-cover").addEventListener("touchend", newAlbum);
+
+document.getElementById("logo").addEventListener("click", colorMode);
+document.getElementById("logo").addEventListener("touchend", colorMode);
